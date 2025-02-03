1e8764dfa4988f183c840075141228b1153eee6d
Ainhize <ainhize.dosunaga@elorrieta-errekamari.com>
2025-02-03 08:28:57 +0100

    aplikazioarekin hasi

---
+++ Bidai_Agentzia/Agentzia_bidaia/src/aplikazioa/Bidai_Agentzia.java
@@ -0,0 +1,43 @@
+package aplikazioa;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Bidai_Agentzia {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Bidai_Agentzia window = new Bidai_Agentzia();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Bidai_Agentzia() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
